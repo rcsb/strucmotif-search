8551365e5554811edc884a876afe00cbd5a49d7c
JonStargaryen <sebastian.bittrich@rcsb.org>
2020-08-17 15:49:45 -0700

    ResidueIdentifier uses label_seq_id

--- strucmotif-search-core/src/main/java/org/rcsb/strucmotif/domain/identifier/ResidueIdentifier.java
+++ strucmotif-search-core/src/main/java/org/rcsb/strucmotif/domain/identifier/ResidueIdentifier.java
@@ -6,23 +6,20 @@ import org.rcsb.strucmotif.domain.structure.ResidueType;
 import java.util.Objects;
 
 /**
- * Uniquely identifies a residue within a chain. Using index and the assembly id of the chain will uniquely identify a
- * residue in the whole structure.
+ * Uniquely identifies a residue within a chain.
  */
 public class ResidueIdentifier {
     private final ResidueType residueType;
     private final int authSeqId;
-    private final String insCode;
     private final int index;
 
-    public ResidueIdentifier(String residueType, int authSeqId, String insCode, int index) {
-        this(ResidueType.ofThreeLetterCode(residueType), authSeqId, insCode, index);
+    public ResidueIdentifier(String residueType, int authSeqId, int index) {
+        this(ResidueType.ofThreeLetterCode(residueType), authSeqId, index);
     }
 
-    public ResidueIdentifier(ResidueType residueType, int authSeqId, String insCode, int index) {
+    public ResidueIdentifier(ResidueType residueType, int authSeqId, int index) {
         this.residueType = residueType;
         this.authSeqId = authSeqId;
-        this.insCode = insCode;
         this.index = index;
     }
 
@@ -42,17 +39,13 @@ public class ResidueIdentifier {
         return authSeqId;
     }
 
-    public String getInsCode() {
-        return insCode;
-    }
-
     public int getIndex() {
         return index;
     }
 
     @Override
     public String toString() {
-        return residueType.getOneLetterCode() + authSeqId + insCode;
+        return residueType.getOneLetterCode() + authSeqId;
     }
 
     @Override
@@ -61,14 +54,14 @@ public class ResidueIdentifier {
         if (o == null || getClass() != o.getClass()) return false;
         ResidueIdentifier that = (ResidueIdentifier) o;
         return authSeqId == that.authSeqId &&
-                index == that.index &&
                 // ignore type to fix microheterogeneity ('fix' by ignoring 2nd/3rd/... position)
 //                residueType == that.residueType &&
-                Objects.equals(insCode, that.insCode);
+                index == that.index;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(/*residueType,*/ authSeqId, insCode, index);
+        // ignore type to fix microheterogeneity ('fix' by ignoring 2nd/3rd/... position)
+        return Objects.hash(/*residueType,*/ authSeqId, index);
     }
 }

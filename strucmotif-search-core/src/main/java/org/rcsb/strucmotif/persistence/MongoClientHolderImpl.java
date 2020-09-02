7fbc8da969982b611812cac8043e6e4add4af3b6
JonStargaryen <sebastian.bittrich@rcsb.org>
2020-09-02 11:34:39 -0700

    honor mongo URI

--- strucmotif-search-core/src/main/java/org/rcsb/strucmotif/persistence/MongoClientHolderImpl.java
+++ strucmotif-search-core/src/main/java/org/rcsb/strucmotif/persistence/MongoClientHolderImpl.java
@@ -2,14 +2,22 @@ package org.rcsb.strucmotif.persistence;
 
 import com.google.inject.Singleton;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
+import org.rcsb.strucmotif.MotifSearch;
 
 @Singleton
 public class MongoClientHolderImpl implements MongoClientHolder {
     private final MongoDatabase database;
 
     public MongoClientHolderImpl() {
-        MongoClient mongoClient = new MongoClient();
+        MongoClient mongoClient;
+        if (MotifSearch.DB_CONNECTION_URI != null) {
+            MongoClientURI uri = new MongoClientURI(MotifSearch.DB_CONNECTION_URI);
+            mongoClient = new MongoClient(uri);
+        } else {
+            mongoClient = new MongoClient();
+        }
         this.database = mongoClient.getDatabase("motif");
 
         // register 'auto'-close of MongoDB connection

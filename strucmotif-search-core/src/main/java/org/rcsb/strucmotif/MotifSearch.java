f94c9174c7081a896815edbc697c65c84120f02d
JonStargaryen <sebastian.bittrich@rcsb.org>
2020-09-02 10:24:56 -0700

    config doc

--- strucmotif-search-core/src/main/java/org/rcsb/strucmotif/MotifSearch.java
+++ strucmotif-search-core/src/main/java/org/rcsb/strucmotif/MotifSearch.java
@@ -49,27 +49,69 @@ import java.util.Properties;
 import java.util.concurrent.ForkJoinPool;
 
 /**
- * The entry point to perform motif searches.
+ * The entry point to perform motif searches. Handles configuration, sets up dependency injection and exposes
+ * configuration.
  */
 public class MotifSearch {
     private static final Logger logger = LoggerFactory.getLogger(MotifSearch.class);
 
+    /**
+     * The maximum distance between residue pairs used for indexing. This subtracted by the 'tolerance' paramater of a
+     * query determines the maximum distance between residues that can be found.
+     */
     public static final double DISTANCE_CUTOFF;
+    /**
+     * <code>DISTANCE_CUTOFF</code> squared.
+     */
     public static final double DISTANCE_CUTOFF_SQUARED;
 
-    public static final Path DATA_ROOT;
+    /**
+     * The base path where all file-system based operations will be performed.
+     */
+    public static final Path PATH_ROOT;
+    /**
+     * Derived from <code>PATH_ROOT</code>.
+     */
     public static final Path ARCHIVE_PATH;
+    /**
+     * Derived from <code>PATH_ROOT</code>.
+     */
     public static final Path LOOKUP_PATH;
+    /**
+     * Derived from <code>PATH_ROOT</code>.
+     */
     public static final Path ARCHIVE_LIST;
+    /**
+     * Derived from <code>PATH_ROOT</code>.
+     */
     public static final Path INDEX_LIST;
+    /**
+     * Derived from <code>PATH_ROOT</code>.
+     */
     public static final Path RESIDUE_LIST;
 
-    public static final String DB_URI;
+    /**
+     * Forces to use a naive file-system based persistence implementation.
+     */
     public static final boolean NO_DB;
+    /**
+     * URI of the database to use. May be <code>null</code> if <code>NO_DB</code> is true or if a local database server
+     * with default settings should be used.
+     */
+    public static final String DB_CONNECTION_URI;
 
-    public static final int THREADS;
+    /**
+     * The number of threads available for processing queries.
+     */
+    public static final int NUMBER_THREADS;
+    /**
+     * A custom thread pool used during execution.
+     */
     public static final ForkJoinPool FORK_JOIN_POOL;
 
+    /**
+     * Standardized number format that can be used throughout the application.
+     */
     public static final DecimalFormat DECIMAL_FORMAT;
 
     private static final MotifSearch INSTANCE = new MotifSearch();
@@ -110,14 +152,29 @@ public class MotifSearch {
         this.queryBuilder = injector.getInstance(QueryBuilder.class);
     }
 
+    /**
+     * Allows direct access to singletons managed by the dependency injection framework.
+     * @param type the API/interface requested
+     * @param <T> the class type
+     * @return the proper implementation as specified in the configuration
+     */
     public static <T> T getInstance(Class<T> type) {
         return INSTANCE.injector.getInstance(type);
     }
 
+    /**
+     * Start a new query using the {@link QueryBuilder}.
+     * @return a fresh builder instance
+     */
     public static QueryBuilder newQuery() {
         return INSTANCE.queryBuilder;
     }
 
+    /**
+     * Format numbers in the specified way.
+     * @param object value to process
+     * @return a formatted number
+     */
     public static String format(Object object) {
         return DECIMAL_FORMAT.format(object);
     }
@@ -131,37 +188,37 @@ public class MotifSearch {
             prop.load(input);
 
             String threadValue = prop.getProperty("number.threads");
-            THREADS = threadValue != null ? Integer.parseInt(threadValue) : Runtime.getRuntime().availableProcessors();
-            logger.info("Will use {} threads", THREADS);
-            FORK_JOIN_POOL = new ForkJoinPool(THREADS);
+            NUMBER_THREADS = threadValue != null ? Integer.parseInt(threadValue) : Runtime.getRuntime().availableProcessors();
+            logger.info("Will use {} threads", NUMBER_THREADS);
+            FORK_JOIN_POOL = new ForkJoinPool(NUMBER_THREADS);
 
             // the cutoff up to which words are detected
             DISTANCE_CUTOFF = Double.parseDouble(prop.getProperty("distance.cutoff"));
             DISTANCE_CUTOFF_SQUARED = DISTANCE_CUTOFF * DISTANCE_CUTOFF;
 
             // the root path of all service data
-            DATA_ROOT = Paths.get(prop.getProperty("path.root"));
-            logger.info("Data root is {}", DATA_ROOT);
+            PATH_ROOT = Paths.get(prop.getProperty("path.root"));
+            logger.info("Data root is {}", PATH_ROOT);
             // an optimized archive - minimal information, maximum IO performance for whole files
-            ARCHIVE_PATH = DATA_ROOT.resolve("archive").resolve("bcif-renum");
+            ARCHIVE_PATH = PATH_ROOT.resolve("archive").resolve("bcif-renum");
             // the location of the lookup
-            LOOKUP_PATH = DATA_ROOT.resolve("lookup");
+            LOOKUP_PATH = PATH_ROOT.resolve("lookup");
             // keeps track of all files for which a reduced/optimized coordinate file exists
-            ARCHIVE_LIST = DATA_ROOT.resolve("archive.list");
+            ARCHIVE_LIST = PATH_ROOT.resolve("archive.list");
             // all structures currently present in the index (may be empty structures - processed but not containing any valid words)
-            INDEX_LIST = DATA_ROOT.resolve("lookup.list");
+            INDEX_LIST = PATH_ROOT.resolve("lookup.list");
             // all structures currently indexed in the component-DB
-            RESIDUE_LIST = DATA_ROOT.resolve("component.list");
+            RESIDUE_LIST = PATH_ROOT.resolve("component.list");
 
             NO_DB = Boolean.parseBoolean(prop.getProperty("no.db"));
             if (NO_DB) {
-                DB_URI = null;
+                DB_CONNECTION_URI = null;
                 logger.info("Falling back to file-system based implementation - enable MongoDB for improved performance");
             } else {
-                DB_URI = prop.getProperty("db.connection.uri", null);
+                DB_CONNECTION_URI = prop.getProperty("db.connection.uri", null);
                 logger.info("MongoDB connection is {}",
-                        DB_URI == null ? "not specified - using local MongoDB server" :
-                                DB_URI.replaceFirst("://.*@", "login"));
+                        DB_CONNECTION_URI == null ? "not specified - using local MongoDB server" :
+                                DB_CONNECTION_URI.replaceFirst("://.*@", "login"));
             }
 
             DECIMAL_FORMAT = new DecimalFormat(prop.getProperty("decimal.format"));

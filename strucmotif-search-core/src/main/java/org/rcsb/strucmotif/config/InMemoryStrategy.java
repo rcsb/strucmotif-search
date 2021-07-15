9851242b13e9f67e449e4a29e243f3a3c968a3f0
Sebastian Bittrich <bittrich@hs-mittweida.de>
2021-07-14 17:10:03 -0700

    TODOs

--- strucmotif-search-core/src/main/java/org/rcsb/strucmotif/config/InMemoryStrategy.java
+++ strucmotif-search-core/src/main/java/org/rcsb/strucmotif/config/InMemoryStrategy.java
@@ -9,7 +9,7 @@ public enum InMemoryStrategy {
      */
     DISABLED,
     /**
-     * Load all data into heap during initialization.
+     * Load all data into heap during initialization. // TODO there are ~300M residues in the archive - investigate an in-memory solution to access coordinates
      */
     HEAP
 }

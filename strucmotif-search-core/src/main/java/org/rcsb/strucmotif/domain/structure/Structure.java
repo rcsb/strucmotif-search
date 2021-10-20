cd3fc99dde8271320516549a10beb3a5d9029229
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2021-10-20 15:24:46 -0700

    cleanup

--- strucmotif-search-core/src/main/java/org/rcsb/strucmotif/domain/structure/Structure.java
+++ strucmotif-search-core/src/main/java/org/rcsb/strucmotif/domain/structure/Structure.java
@@ -7,7 +7,6 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 

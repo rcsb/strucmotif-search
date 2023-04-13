b0e72f5eb8000c712c42f4a11678860669818815
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2023-04-13 11:38:33 -0700

    add custom FailureAnalyzer if read/write fails during postconstruct

---
+++ strucmotif-search-core/src/main/java/org/rcsb/strucmotif/NoSuchFileFailureAnalyzer.java
@@ -0,0 +1,30 @@
+package org.rcsb.strucmotif;
+
+import org.springframework.beans.factory.UnsatisfiedDependencyException;
+import org.springframework.boot.diagnostics.AbstractFailureAnalyzer;
+import org.springframework.boot.diagnostics.FailureAnalysis;
+
+import java.nio.file.NoSuchFileException;
+
+public class NoSuchFileFailureAnalyzer extends AbstractFailureAnalyzer<UnsatisfiedDependencyException> {
+    @Override
+    protected FailureAnalysis analyze(Throwable rootFailure, UnsatisfiedDependencyException cause) {
+        Throwable mostSpecific = cause.getMostSpecificCause();
+        if (mostSpecific instanceof NoSuchFileException) {
+            return new FailureAnalysis(getDescription(cause), getAction((NoSuchFileException) mostSpecific), cause);
+        }
+
+        // this causes the standard, super-verbose stack trace to kick in
+        return null;
+    }
+
+    private String getDescription(UnsatisfiedDependencyException ex) {
+        return String.format("The bean '%s' could not be created, the root cause is '%s'.",
+                ex.getBeanName(), ex.getMostSpecificCause());
+    }
+
+    private String getAction(NoSuchFileException ex) {
+        return String.format("Make sure that the directory containing '%s' exists and this process has the necessary read/write permissions.",
+                ex.getFile());
+    }
+}

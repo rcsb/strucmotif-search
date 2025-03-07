ee8edef5282ef7998a530d293345247e0c81338d
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2025-03-07 15:00:00 -0800

    backwards compatibility

--- strucmotif-search-core/src/main/java/org/rcsb/strucmotif/io/StructureWriter.java
+++ strucmotif-search-core/src/main/java/org/rcsb/strucmotif/io/StructureWriter.java
@@ -3,9 +3,18 @@ package org.rcsb.strucmotif.io;
 import org.rcsb.cif.schema.mm.MmCifFile;
 
 /**
- * The contract to write optimized structure data that don't contain non-polymers, hydrogen, model afters #1 etc.
+ * The contract to write optimized structure data that don't contain non-polymers, hydrogen, non-selected models etc.
  */
 public interface StructureWriter {
+    /**
+     * Converts a structure data to its binary representation.
+     * @param source a MmCifFile to process
+     * @return a byte[] with the file content or null if the structure would be empty
+     */
+    default byte[] write(MmCifFile source) {
+        return write(source, StructureDataProvider.DEFAULT_MODEL_IDENTIFIER);
+    }
+
     /**
      * Converts a structure data to its binary representation.
      * @param source a MmCifFile to process
